9611022b88d395db444c2cab55e8c2871c47ee5d
Utkarsh Gangwar <49022033+Utkarsh-Gangwar@users.noreply.github.com>
2019-10-14 07:11:39 +0530

    Add files via upload

--- image.py
+++ image.py
@@ -1,14 +1,16 @@
 import cv2
-face_cascade = cv2.CascadeClassifier("F:\\py\\haarcascade_frontalface_default.xml")
-img = cv2.imread("F:\\py\\4.jpeg")
+#you can get cascade flile on link : https://github.com/opencv/opencv/tree/master/data/haarcascades
+face_cascade = cv2.CascadeClassifier("F:\\py\\haarcascade_frontalface_default.xml")#give path of cascade file
+img = cv2.imread("F:\\py\\4.jpeg")#give path of image file
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.9 , minNeighbors = 5)
 
 for x,y,w,h in faces:
     img - cv2.rectangle (img, (x,y), (x+w,y+h), (255,0,0),2)
 
-resized = cv2.resize(img, (int(img.shape[1]), int(img.shape[0])))
-cv2.imshow("Gray", resized)
+#resized = cv2.resize(img, (int(img.shape[1]), int(img.shape[0])))
+#cv2.imshow("Greay", resize)
+cv2.imshow("4", img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
