5d8426797abe163ca4709c8931e814b4dc0373a9
Utkarsh Gangwar <49022033+Utkarsh-Gangwar@users.noreply.github.com>
2019-10-15 12:09:59 +0530

    Update image.py

--- image.py
+++ image.py
@@ -3,7 +3,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier("F:\\py\\haarcascade_frontalface_default.xml")#give path of cascade file
 img = cv2.imread("F:\\py\\4.jpeg")#give path of image file
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.9 , minNeighbors = 5)
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.05 , minNeighbors = 5)
 
 for x,y,w,h in faces:
     img - cv2.rectangle (img, (x,y), (x+w,y+h), (255,0,0),2)
